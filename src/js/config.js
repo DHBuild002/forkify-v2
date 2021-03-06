7440e6806844b49170171a64199327cb82ef09e9
Dan Haddock <dhbuild.002@gmail.com>
2021-03-06 10:56:37 +0000

    Removing the cache of my config file

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SECONDS = 10; 
-export const RES_PER_PAGE = 10;
-export const API_KEY = 'd09831b3-1334-4de1-ae01-ffb500d798f5';
-export const MODAL_CLOSE_SEC = 2.5;
